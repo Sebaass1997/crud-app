b76a92ab73ebb7709dc8c7c5c20320d0c8958d44
Sebastian <sebaperino@gmail.com>
2021-08-11 00:13:03 -0300

    index routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { UsuariosComponent } from './usuarios/usuarios.component';
 import { AddUsuarioComponent } from './usuarios/add-usuario/add-usuario.component';
 
 const appRoutes = [
+  { path: '',   redirectTo: '/usuarios', pathMatch: 'full' },
   { path: 'login', component: LoginComponent,  pathMatch: 'full'},
   { path: 'usuarios', component: UsuariosComponent,  pathMatch: 'full'},
   { path: 'usuarios/add', component: AddUsuarioComponent,  pathMatch: 'full'}
